a1393e2fabef8850e86cfde712cbac472a6ed861
Ricardo Cortes <rcr.786@gmail.com>
2025-01-29 13:06:03 -0600

    new build

--- public/build/assets/PrimaryButton-BSLsR4H7.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./app-Be7-NWdQ.js";function u({className:n="",disabled:t,children:r,...o}){return a.jsx("button",{...o,className:`btn btn-neutral ${t&&"opacity-25"} `+n,disabled:t,children:r})}export{u as P};
